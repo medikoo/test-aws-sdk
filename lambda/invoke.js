e482038a1978d398229cfbc9fba4bcc6947b193b
Mariusz Nowak <medyk@medikoo.com>
2021-10-04 15:04:15 +0200

    Improve invoke

--- lambda/invoke.js
+++ lambda/invoke.js
@@ -8,4 +8,14 @@ const AWS = require("aws-sdk");
 
 const lambda = new AWS.Lambda({ region: process.env.AWS_REGION });
 
-lambda.invoke({ FunctionName: process.argv[2] }).promise().then(result => { console.log(result); });
+lambda
+	.invoke({ FunctionName: process.argv[2] })
+	.promise()
+	.then(result => {
+		console.log(result);
+		try {
+			const payload = JSON.parse(result.Payload);
+			console.log(payload);
+			console.log(JSON.parse(payload.body));
+		} catch {}
+	});
