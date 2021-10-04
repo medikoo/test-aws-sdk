9b86619875ba83b22dbc9a1cc7acc829ebf0fcc3
Mariusz Nowak <medyk@medikoo.com>
2021-10-04 15:04:32 +0200

    Separate docker lambda

--- lambda/function/index.js
+++ lambda/function/index.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports.handler = (event, context, callback) => {
-	callback(null, { statusCode: 200, body: JSON.stringify({ message: "Hello from Docker 2!" }) });
+	callback(null, { statusCode: 200, body: JSON.stringify({ event, env: process.env, context }) });
 };
