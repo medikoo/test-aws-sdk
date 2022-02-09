0bb5fea88267a55a1f3e29bbac439eb6976cfef6
Mariusz Nowak <medyk@medikoo.com>
2022-02-09 15:27:12 +0100

    Support packaging nested structures

--- lambda/lib/resolve-function-zip-buffer.js
+++ lambda/lib/resolve-function-zip-buffer.js
@@ -1,14 +1,16 @@
 "use strict";
 
-const path   = require("path")
-    , fs     = require("fs").promises
-    , AdmZip = require("adm-zip");
+const path    = require("path")
+    , readdir = require("fs2/readdir")
+    , AdmZip  = require("adm-zip");
 
 module.exports = async functionRoot => {
-	const lambdaFiles = await fs.readdir(functionRoot);
+	const lambdaFiles = await readdir(functionRoot, { depth: Infinity, type: { file: true } });
 	const zip = new AdmZip();
 
-	for (const file of lambdaFiles) zip.addLocalFile(path.resolve(functionRoot, file));
+	for (const file of lambdaFiles) {
+		zip.addLocalFile(path.resolve(functionRoot, file), path.dirname(file));
+	}
 
 	return zip.toBuffer();
 };
