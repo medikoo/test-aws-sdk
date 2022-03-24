6dcca59335166c9d564869ca094e2971b6c3d898
Mariusz Nowak <medyk@medikoo.com>
2022-03-24 16:56:57 +0100

    Fix dotenv resolution

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 "use strict";
 
 require("essentials");
-require("dotenv").config();
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 require("log-node")();
 
 if (!process.env.AWS_REGION) process.env.AWS_REGION = "us-east-1";
